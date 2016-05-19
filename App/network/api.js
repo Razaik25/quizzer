52dd1d6e0717740a2599f00c86ef666180eed201
Razaik Boparai <razaikb@gmail.com>
2016-05-18 22:34:35 -0400

    refactoring api calls

--- App/network/api.js
+++ App/network/api.js
@@ -32,17 +32,30 @@ var api = {
   updateUser(email,category,score){
     email = email.toLowerCase().trim();
     category = category.toLowerCase().trim();
+    console.log(email,category,score);
     // commenting out the localhost connection and adding heroku server
-    // var url = `http://localhost:3000/users/update/${email}`;
-    var url = `https://quizzer-api.herokuapp.com/users/update/${email}`;
+    var url = `http://localhost:3000/users/update/${email}`;
+    // var url = `https://quizzer-api.herokuapp.com/users/update/${email}`;
+    var data = {
+      email: email,
+      category: category,
+      score: score.toString()
+    };
     return fetch(url, {
       method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
       body: JSON.stringify({
         email: email,
         category: category,
         score: score
       })
-    }).then((res) => res.json());
+    }).then((res) => res.json())
+      .catch((ex)  => {
+        console.log('parsing failed', ex)
+      });
   }
 
 };
